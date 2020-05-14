c6460cb48028e6e7cbdba5642fe431d1bab9992e
drebollo16 <drebollo16@byui.edu>
2020-05-13 19:18:34 -0600

    commit changes

---
+++ lesson04/js/date.js
@@ -0,0 +1,9 @@
+let set = new Date(document.lastModified);
+let month = set.getMonth() + 1;
+let date = set.getDate();
+let year = set.getFullYear();
+let hour = set.getHours();
+let mintue = set.getMinutes();
+let second = set.getSeconds();
+let update = month + "/" + date + "/" + year + " " + hour + ":" + mintue + ":" + second;
+document.getElementById("update").innerHTML = update;
