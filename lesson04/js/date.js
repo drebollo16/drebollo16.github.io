814b711f34b50699cc5db39458df91b561189346
drebollo16 <drebollo16@byui.edu>
2020-05-15 21:33:29 -0600

    changes

--- lesson04/js/date.js
+++ lesson04/js/date.js
@@ -1,9 +1,4 @@
-let month = set.getMonth() + 1;
-let date = set.getDate();
-let year = set.getFullYear();
-let hour = set.getHours();
-let mintue = set.getMinutes();
-let second = set.getSeconds();
-let update = month + "/" + date + "/" + year + " " + hour + ":" + mintue + ":" + second;
-document.getElementById("update").innerHTML = update;
-console.log("date");
+let date = new Date();
+let todaydate = date.getDate();
+
+document.getElementById("update").innerHTML = date.toDateString();
