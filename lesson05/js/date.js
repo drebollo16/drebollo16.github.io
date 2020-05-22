91758f9aa69ddc2c414009e589734edb09675ffa
drebollo16 <drebollo16@byui.edu>
2020-05-22 11:55:08 -0600

    changes

--- lesson05/js/date.js
+++ lesson05/js/date.js
@@ -2,3 +2,13 @@ let date = new Date();
 let todaydate = date.getDate();
 
 document.getElementById("update").innerHTML = date.toDateString();
+
+
+// Banner for Fridays
+let d = new Date();
+const banner = document.getElementById("alertbanner");
+if (d.getDate() == 3) {
+    banner.style.display = "block";
+} else {
+    banner.style.display = "none";
+}
--- lesson05/js/response.js
+++ lesson05/js/response.js
@@ -2,14 +2,3 @@ const hambutton = document.querySelector('.menusign');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
-
-
-
-// Banner for Fridays
-let d = new Date();
-const banner = document.getElementById("banner");
-if (d.getDate() == 3) {
-    banner.style.display = "block";
-} else {
-    banner.style.display = "none";
-}
