0e54bb27200e860421cbd8ac61cdcfa483616bd4
drebollo16 <drebollo16@byui.edu>
2020-05-22 07:01:01 -0600

    submit

--- lesson05/js/response.js
+++ lesson05/js/response.js
@@ -2,3 +2,14 @@ const hambutton = document.querySelector('.menusign');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
+
+
+
+// Banner for Fridays
+let d = new Date();
+const banner = document.getElementById("banner");
+if (d.getDate() == 3) {
+    banner.style.display = "block";
+} else {
+    banner.style.display = "none";
+}
