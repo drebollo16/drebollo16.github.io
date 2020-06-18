6e8484f0287c5a4557fe91f0f9decc88a6f2def5
drebollo16 <drebollo16@byui.edu>
2020-06-17 21:32:24 -0600

    changes

---
+++ lesson09/js/prophets.js
@@ -0,0 +1,35 @@
+ const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+ fetch(requestURL)
+     .then(function(response) {
+         return response.json();
+     })
+     .then(function(jsonObject) {
+         //console.table(jsonObject); // temporary checking for valid response and data parsing
+
+         const prophets = jsonObject['prophets'];
+         // const utah = prophets.filter(prophet => (prophet.birthplace == "Utah")); //example for main page filter
+         prophets.forEach(prophets => {
+
+
+             // for (let i = 0; i < prophets.length; i++) {
+             let card = document.createElement('section');
+
+             let h2 = document.createElement('h2');
+             let birthplace = document.createElement('div');
+             let image = document.createElement('img');
+             let birthdate = document.createElement('birth');
+
+             h2.innerHTML = `${prophets.name} ${prophets.lastname}`;
+             image.setAttribute('src', prophets.image);
+
+
+             birthdate.innerHTML = `<strong>Date of Birth: ${prophets.birthdate}</strong>`;
+             birthplace.innerHTML = `<strong>Place of Birth: ${prophets.birthplace}</strong>`;
+             card.appendChild(h2);
+             card.appendChild(image);
+             card.appendChild(birthdate);
+             card.appendChild(birthplace);
+             document.querySelector('div.cards').appendChild(card);
+         });
+     });
