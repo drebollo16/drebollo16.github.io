4dbfd0708d56378b8625bd75924f47f0e1d85b48
drebollo16 <drebollo16@byui.edu>
2020-06-20 19:27:39 -0600

    changes

--- lesson09/js/preston9.js
+++ lesson09/js/preston9.js
@@ -15,27 +15,31 @@ fetch(requestURL)
             if (towns.name == 'Preston' || towns.name == 'Fish Haven' || towns.name == 'Soda Springs') {
 
                 let card = document.createElement('section');
-                let h2 = document.createElement('div');
-                let motto = document.createElement('div');
-                let yearFounded = document.createElement('div');
-                let currentPopulation = document.createElement('div');
+                let h2 = document.createElement('h2');
+                let display = document.createElement('div');
+                let motto = document.createElement('h3');
+                let yearFounded = document.createElement('h3');
+                let currentPopulation = document.createElement('h3');
                 let photo = document.createElement('img');
-                let name = document.createElement('div');
-                let averageRainfall = document.createElement('div');
+                let name = document.createElement('h3');
+                let averageRainfall = document.createElement('h3');
 
                 photo.setAttribute('src', towns.photo);
                 motto.innerHTML = `<i>${towns.motto}<br>`;
+                display.setAttribute('class', 'display');
 
                 currentPopulation.innerHTML = `Population: ${towns.currentPopulation}<br>`;
                 yearFounded.innerHTML = `Year Founded: ${towns.yearFounded}<br>`;
                 h2.innerHTML = `<h2>${towns.name}</h2>`;
                 averageRainfall.innerHTML = `Average Rainfall :${towns.averageRainfall}`;
-                card.appendChild(name);
-                card.appendChild(h2);
-                card.appendChild(motto);
-                card.appendChild(yearFounded);
-                card.appendChild(currentPopulation);
-                card.appendChild(averageRainfall);
+                card.appendChild(display);
+                display.appendChild(name);
+                display.appendChild(h2);
+                display.appendChild(motto);
+                display.appendChild(yearFounded);
+                display.appendChild(currentPopulation);
+                display.appendChild(averageRainfall);
+
                 card.appendChild(photo);
 
                 document.querySelector('div.cards').appendChild(card);
