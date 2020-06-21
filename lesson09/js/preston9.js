5ea6b3d725cfd123d6b6ff420e9f4f8f028b7025
drebollo16 <drebollo16@byui.edu>
2020-06-20 22:31:37 -0600

    changes

--- lesson09/js/preston9.js
+++ lesson09/js/preston9.js
@@ -15,26 +15,26 @@ fetch(requestURL)
             if (towns.name == 'Preston' || towns.name == 'Fish Haven' || towns.name == 'Soda Springs') {
 
                 let card = document.createElement('section');
-                let h2 = document.createElement('h2');
+
                 let display = document.createElement('div');
                 let motto = document.createElement('h3');
                 let yearFounded = document.createElement('h3');
                 let currentPopulation = document.createElement('h3');
                 let photo = document.createElement('img');
-                let name = document.createElement('h3');
+                let name = document.createElement('h2');
                 let averageRainfall = document.createElement('h3');
 
                 photo.setAttribute('src', towns.photo);
-                motto.innerHTML = `<i>${towns.motto}<br>`;
+                motto.innerHTML = `<i>${towns.motto}`;
                 display.setAttribute('class', 'display');
 
-                currentPopulation.innerHTML = `Population: ${towns.currentPopulation}<br>`;
-                yearFounded.innerHTML = `Year Founded: ${towns.yearFounded}<br>`;
-                h2.innerHTML = `<h2>${towns.name}</h2>`;
+                currentPopulation.innerHTML = `Population: ${towns.currentPopulation}`;
+                yearFounded.innerHTML = `Year Founded: ${towns.yearFounded}`;
+                name.innerHTML = `${towns.name}`;
                 averageRainfall.innerHTML = `Average Rainfall :${towns.averageRainfall}`;
                 card.appendChild(display);
                 display.appendChild(name);
-                display.appendChild(h2);
+
                 display.appendChild(motto);
                 display.appendChild(yearFounded);
                 display.appendChild(currentPopulation);
