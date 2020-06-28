6cad8677dc8712ef936294e5ff33ea91bcd17892
drebollo16 <drebollo16@byui.edu>
2020-06-27 18:48:18 -0600

    date changes json

--- lesson10/js/preston10.js
+++ lesson10/js/preston10.js
@@ -1,32 +1,57 @@
- const apiURLs = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=8ef3acee6058752fb9adc42140902cd6&units=imperial";
- fetch(apiURLs)
-     .then((response) => response.json())
-     .then((jsObject) => {
-         console.log(jsObject);
-
-         const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('12:00:00'));
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=8ef3acee6058752fb9adc42140902cd6&units=imperial";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
 
 
-         console.log(fivedayforecast);
-         const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-         let day = 0;
+        document.getElementById('currently').textContent = jsObject.weather[0].description;
+        let high = document.getElementById('high').innerHTML = jsObject.main.temp_max + '&deg;F';
+        document.getElementById('humid').innerHTML = jsObject.main.humidity + '%';
+        let windspeed = document.getElementById('windspeed').innerHTML = jsObject.wind.speed + 'mph';
+
+        //the formula to calculate the wind chill factor is LaTeX: 
+        // f=35.74+0.6215\:t-35.75\:s^{0.16}+0.4275\:t\:s^{0.16}
+        let ws = Math.pow(windspeed, 0.16);
+
+        let windchill = 35.74 + 0.6215 * high - 35.75 * ws + 0.4275 * high * ws;
+
+        if (high <= 50 && windspeed > 3) {
+
+            document.getElementById('windchill').innerHTML = Math.round(windchill);
+        } else {
+
+            document.getElementById('windchill').innerHTML = "N/A";
+        }
 
 
-         fivedayforecast.forEach(forecast => {
+    });
 
-             let d = new Date(forecast.dt_txt);
-             document.getElementById(`forecast${day+1}`).textContent = forecast.main.temp;
 
-             document.getElementById(`dayofweek${day+1}`).textContent = weekday[d.getDay()];
-             const icon = document.querySelector('img');
-             const imagesrc = 'https://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png';
-             const desc = forecast.weather[0].description;
+const apiURLs = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=8ef3acee6058752fb9adc42140902cd6&units=imperial";
+fetch(apiURLs)
+    .then((response) => response.json())
+    .then((jsObject) => {
 
-             // document.getElementById('imagesrc').textContent = imagesrc;
-             document.getElementById('icon').setAttribute('src', imagesrc);
-             document.getElementById('icon').setAttribute('alt', desc);
-             day++;
+        const forecast = jsObject['list'];
+        console.log(forecast);
+        console.table(forecast);
 
-         });
+        let weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let day = 0;
 
-     });
+        for (let i = 6; i < forecast.length; i += 8) {
+
+            const days = new Date(forecast[i].dt_txt);
+            const date = weekday[days.getDay()];
+
+            document.getElementById(`dayofweek${day+i}`).textContent = date;
+
+            const imagesrc = 'https://openweathermap.org/img/w/' + forecast[i].weather[0].icon + '.png';
+            const desc = forecast[i].weather[0].description;
+
+            document.getElementById(`icon${i}`).setAttribute('src', imagesrc);
+            document.getElementById(`icon${i}`).setAttribute('alt', desc);
+            document.getElementById(`forecast${i}`).textContent = forecast[i].main.temp;
+        }
+
+    });
