50cc1c74cd1def4f0c394dbb0d113232d7c60f10
drebollo16 <drebollo16@byui.edu>
2020-07-01 11:07:22 -0600

    changes

---
+++ lesson11/js/weatherapi.js
@@ -0,0 +1,19 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=8ef3acee6058752fb9adc42140902cd6&units=imperial";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        const currentTemp = document.querySelector('current-temp');
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        const icon = document.querySelector('img');
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        const desc = jsObject.weather[0].description;
+        document.getElementById('imagesrc').textContent = imagesrc;
+        document.getElementById('icon').setAttribute('src', imagesrc);
+        document.getElementById('icon').setAttribute('alt', desc);
+
+
+
+    });
