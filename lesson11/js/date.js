1ba035333ddc06a969e78669aece6da3012df50c
drebollo16 <drebollo16@byui.edu>
2020-07-04 13:33:56 -0600

    date changes

--- lesson11/js/date.js
+++ lesson11/js/date.js
@@ -3,7 +3,6 @@ let monthName = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"
 ];
 let date = new Date();
-let todaydate = date.getDate();
 let weeks = dayWeek[date.getDay()];
 let months = monthName[date.getMonth()];
 document.getElementById("update").innerHTML = weeks + "," + " " + date.getDate() + " " + months + " " + date.getFullYear();
