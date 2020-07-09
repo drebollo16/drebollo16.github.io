a57ff0e4295e72b7adb2ec09586011b4369f0940
drebollo16 <drebollo16@byui.edu>
2020-07-08 22:07:19 -0600

    changes

--- Final-Scoots/js/scoots.js
+++ Final-Scoots/js/scoots.js
@@ -1,27 +1,10 @@
-const apiURLs = "drebollo16.github.io/Final-Scoots/json";
+const apiURLs = "https://drebollo16.github.io/Final-Scoots/scootsjson.json";
 fetch(apiURLs)
     .then((response) => response.json())
     .then((jsObject) => {
 
-        const forecast = jsObject['list'];
+        const forecast = jsObject['pricechart'];
 
 
-        let weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        let day = 0;
-
-        for (let i = 6; i < forecast.length; i += 8) {
-
-            const days = new Date(forecast[i].dt_txt);
-            const date = weekday[days.getDay()];
-
-            document.getElementById(`dayofweek${day+i}`).textContent = date;
-
-            const imagesrc = 'https://openweathermap.org/img/w/' + forecast[i].weather[0].icon + '.png';
-            const desc = forecast[i].weather[0].description;
-
-            document.getElementById(`icon${i}`).setAttribute('src', imagesrc);
-            document.getElementById(`icon${i}`).setAttribute('alt', desc);
-            document.getElementById(`forecast${i}`).textContent = forecast[i].main.temp;
-        }
 
     });
