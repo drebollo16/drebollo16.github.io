e704cd8f580efa978b13db76ea5d84c67daf98f0
drebollo16 <drebollo16@byui.edu>
2020-07-08 23:17:44 -0600

    changes

--- Final-Scoots/js/scoots.js
+++ Final-Scoots/js/scoots.js
@@ -3,15 +3,17 @@ fetch(apiURLs)
     .then((response) => response.json())
     .then((jsObject) => {
 
-        const rentaltype = jsObject['pricechart'];
+        const pricechart = jsObject['pricechart'];
 
-        for (let i = 0; i < rentaltype.length; i++) {
+        for (let i = 0; i < pricechart.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
+            let p = document.createElement('p');
 
-            h2.textContent = rentaltype[i].rentaltype;
-
+            h2.textContent = " Rental Type " + pricechart[i].rentaltype;
+            p.textContent = pricechart[i].maxperson + " Max Person";
             card.appendChild(h2);
+            card.appendChild(p);
 
             document.querySelector('div.cards').appendChild(card);
         }
