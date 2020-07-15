f4c0c139f4b75631a9af4bed116b54de26a691fe
drebollo16 <drebollo16@byui.edu>
2020-07-15 16:14:18 -0600

    changes

--- Final-Scoots/js/scoots.js
+++ Final-Scoots/js/scoots.js
@@ -1,24 +1,27 @@
-const apiURLs = "https://drebollo16.github.io/Final-Scoots/scootsjson.json";
-fetch(apiURLs)
-    .then((response) => response.json())
-    .then((jsObject) => {
+const requestURL = "https://drebollo16.github.io/Final-Scoots/scootsjson.json";
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
 
-        const pricechart = jsObject['pricechart'];
+        const pricechart = jsonObject['pricechart'];
 
-        for (let i = 0; i < pricechart.length; i++) {
+        pricechart.forEach(pricechart => {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
             let p = document.createElement('p');
             let photo = document.createElement('img');
 
-            photo.setAttribute('src', pricechart[i].photo);
-            photo.setAttribute('alt', pricechart[i].photo);
+            photo.setAttribute('src', pricechart.photo);
+            photo.setAttribute('alt', pricechart.photo);
 
-            h2.textContent = " Rental Type " + pricechart[i].rentaltype;
-            p.textContent = " Max Person " + pricechart[i].maxperson;
+            h2.textContent = " Rental Type " + pricechart.rentaltype;
+            p.textContent = " Max Person " + pricechart.maxperson;
             card.appendChild(h2);
             card.appendChild(p);
             card.appendChild(photo);
             document.querySelector('div.cards').appendChild(card);
-        }
+
+        });
     });
